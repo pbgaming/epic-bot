b3c63857d7cd58f2f867cee38960f8adba03a246
pbgaming <44928010+pbgaming@users.noreply.github.com>
2018-12-05 22:50:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,6 +229,27 @@ var prefix = "$";
 
 });
 
+client.on('message', message => {
+    var args = message.content.toLowerCase().split(' ');
+    var command = args[0];
+    var prefix = '$';
+    var wordsSay = message.content.split(' ').slice(1).join(' ');
+   
+    if(command == prefix + 'say') {
+        var sayRole = message.guild.roles.find(r => r.name === 'say');
+        if(!sayRole) return message.channel.send('لا استطيع ايجاد رتبة `say` ');
+        if(!message.member.roles.has(sayRole.id)) return message.channel.send('يجب ان تتوفر لديك رتبة `say`');
+        if(!wordsSay) return message.channel.send(`***EX :*** ${prefix}say Hello World! `);
+       
+        message.delete();
+        let sayE = new Discord.RichEmbed()  
+        .setColor('RANDOM')
+        .setDescription(`**${wordsSay}**`)  
+       
+        message.channel.send(sayE);
+    }
+});
+
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Cyhper Script By : DREAM`);
