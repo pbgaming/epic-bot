fa4d573a4388382412fd76e42f279fb8e1c19ec4
pbgaming <44928010+pbgaming@users.noreply.github.com>
2018-12-13 00:38:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -267,6 +267,14 @@ client.on('message', message => {
     }
 });     
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Cyhper Script By : DREAM`);
